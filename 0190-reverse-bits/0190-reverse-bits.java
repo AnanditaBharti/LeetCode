d60b847a855a35cd0d4a91269a834fb3919e7797
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2024-07-29 00:12:20 +0530

    Time: 0 ms (100%), Space: 41.7 MB (36.46%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,15 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        // int m=Integer.MAX_VALUE;
+        // m=m^n;
+        // return m;
+        int res=0;
+        for(int i=0;i<32;i++)
+        {
+            res=(res<<1)|(n&1);
+            n=n>>1;
+        }
+        return res;
+    }
+}
