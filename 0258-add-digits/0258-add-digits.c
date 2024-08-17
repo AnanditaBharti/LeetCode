50cf7faf8e2b2abe9b68d3d3e16ea9fa646a4500
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2024-08-17 21:17:29 +0530

    Time: 0 ms (100%), Space: 8.3 MB (15.68%) - LeetHub

--- 0258-add-digits/0258-add-digits.c
+++ 0258-add-digits/0258-add-digits.c
@@ -1,14 +1,8 @@
 int addDigits(int num) {
-    while(num/10!=0)
-    {
-        int temp=num;
-        int sum=0;
-        while(temp!=0)
-        {
-            sum+=temp%10;
-            temp/=10;
-        }
-        num=sum;
-    }
-    return num;
+    if(num==0)
+      return 0;
+    else if(num%9==0)
+      return 9;
+    else
+      return num%9;
 }
