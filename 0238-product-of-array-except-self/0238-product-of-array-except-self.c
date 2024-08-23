f475fb3e0e44a677b73e1aaf0ec678b538bbed30
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2024-08-23 10:40:27 +0530

    Time: 150 ms (32.48%), Space: 30.4 MB (27.97%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.c
@@ -0,0 +1,27 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    int product=1;
+    for(int i=0;i<numsSize;i++)
+    {
+        product*=nums[i];
+    }
+    int* result=(int*)malloc(sizeof(int)*numsSize);
+    // returnSize=(int*)malloc(sizeof(int)*numsSize);
+    for(int i=0;i<numsSize;i++)
+    {
+        if (nums[i]!=0)
+           result[i]=product/nums[i];
+        else{
+           result[i]=1;
+           for(int j=0;j<numsSize;j++)
+           {
+               if(j!=i)
+                   result[i]*=nums[j];
+           }
+        }
+    }
+    *returnSize=numsSize;
+    return result;
+}
