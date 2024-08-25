2a8439eb5cdebaa6adc4c508fac1041dcb7203bb
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2024-08-25 10:29:07 +0530

    Time: 846 ms (16.84%), Space: 32.6 MB (12.11%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,24 +1,7 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        # dict1={}
-        # dict2={}
-        # i=nums[0]-1
-        # for j in nums:
-        #     dict2={}
-        #     for k in dict1:
-        #         if j>k:
-        #             a=dict1[k]
-        #             dict2.update({j:a+1})
-        #     dict1.update(dict2)
-        #     if(not dict1 ):
-        #         dict1.update({j:1})
-        # for j in dict1.values():
-        #     if j>=3:
-        #         return True
-        # return False
-        a,b,c=max(nums)+1,max(nums)+1,max(nums)+1 #a=1,b=2,c=6
-        #print(a,b,c)
-        for j in nums: #j=3
+        a,b,c=max(nums)+1,max(nums)+1,max(nums)+1
+        for j in nums:
             if j<a:
                 a=j
             if j>a and j<b:
