3fc33d421ab2bc502b4360ee937687fc7f89b0d3
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2024-08-28 20:31:19 +0530

    Time: 10 ms (10.91%), Space: 44.7 MB (7.32%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int compress(char[] chars) {
+        char curr=chars[0];
+        int count=1;
+        String res="";
+        for(int i=1;i<chars.length;i++)
+        {
+            if(chars[i]==curr)
+            {
+                count++;
+            }
+            else if(count==1)
+            {
+                res+=curr;
+                curr=chars[i];
+            }
+            else
+            {
+                res+=curr;
+                res+=count;
+                count=1;
+                curr=chars[i];
+            }
+        }
+        if(count==1)
+        {
+            res+=curr;
+        }
+        else
+        {
+                res+=curr;
+                res+=count;
+                
+        }
+        // System.out.println(res);
+        for (int i=0;i<res.length();i++)
+        {
+            chars[i]=res.charAt(i);
+        }
+        return res.length();
+    }
+}
