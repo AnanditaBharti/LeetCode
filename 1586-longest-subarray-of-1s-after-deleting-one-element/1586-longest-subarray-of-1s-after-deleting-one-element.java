b0565ff3b80cca592dba2d035744e03e2ec8cd2d
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2024-10-28 22:02:10 +0530

    Time: 3 ms (77.28%), Space: 57.2 MB (10.66%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,17 +1,15 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int leftPointer=0;//4
-        // int rightPointer=0;
-        boolean zeroDropped=false;//true//false//true
-        int droppedZeroIndex=0;//3//5
+        int leftPointer=0;
+        boolean zeroDropped=false;
+        int droppedZeroIndex=0;
         int n = nums.length;
         int maxLength=0;//2
-        boolean deletedOnce=false;//true
-        for(int i=0;i<n;i++)//6
+        boolean deletedOnce=false;
+        for(int i=0;i<n;i++)
         {
             if(nums[i]==1)
             {
-                // rightPointer=i;
                 if(nums[leftPointer]==0){
                    leftPointer=i;
                    zeroDropped=false;
@@ -27,7 +25,6 @@ class Solution {
                 }
                 if(temp>maxLength){
                    maxLength = temp;
-                //    System.out.println(leftPointer+" "+i);
                 }
             }
             else if(!zeroDropped)
@@ -40,58 +37,8 @@ class Solution {
             {
                 leftPointer=droppedZeroIndex+1;
                 droppedZeroIndex=i;
-                // zeroDropped=false;
             }
         }
         return (deletedOnce)?maxLength:maxLength-1;
-    //     boolean start=false;
-    //     int res=0;
-    //     int temp=0;
-    //     for(int i=0;i<n;i++)
-    //     {
-    //         if(nums[i]==0)
-    //         {
-    //             if(start)
-    //             {
-    //                 if(!zero){
-    //                   zero=true;
-    //                   right=i;
-    //                 }
-    //                 else
-    //                 {
-    //                     while(nums[left]==1)
-    //                     {
-    //                         left++;
-
-    //                     }
-    //                     left++;
-    //                 }
-    //             }
-                
-    //         }
-    //         else
-    //         {
-    //             temp++;
-    //             if(!start)
-    //             {
-    //                 start=true;
-    //             }
-    //             right=i;
-    //         }
-    //         temp=right-left+1;
-    //         if(zero)
-    //         {
-    //             // if(nums[right]==1)
-    //               temp--;
-                  
-    //         }
-    //         if(temp>res)
-    //           res=temp;
-            
-    //     }
-    //     int res2=right-left;
-    //     if(!zero)
-    //       res2--;
-    //     return res;
     }
 }
