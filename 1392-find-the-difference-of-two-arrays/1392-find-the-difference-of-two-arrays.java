fab8ba808c96ba62663f044d32f33cbd7699459c
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2025-02-05 17:30:15 +0530

    Time: 18 ms (15.47%), Space: 45.8 MB (11.59%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.java
@@ -7,23 +7,12 @@ class Solution {
         Set<Integer> integersInNums2 = Arrays.stream(nums2) 
                                                        .boxed()
                                                        .collect(Collectors.toSet());
-        // Iterator<Integer> iterate = distinctIntegersInNums1.iterator() ;
-        // while(iterate.hasNext()){
-        //     Integer value = iterate.next() ;
-        //     if(distinctIntegersInNums2.contains(value)){
-        //         iterate.remove();
-        //         distinctIntegersInNums2.remove(value);
-        //     }
-        // }
+       
         Set<Integer> distinctIntegersInNums1 = new HashSet<>(integersInNums1);
         Set<Integer> distinctIntegersInNums2 = new HashSet<>(integersInNums2);
         distinctIntegersInNums1.removeAll(integersInNums2) ;
         distinctIntegersInNums2.removeAll(integersInNums1) ;
-        // List<Integer> x = new ArrayList<>(distinctIntegersInNums1) ;
-        // List<Integer> y = new ArrayList<>(distinctIntegersInNums2) ;
         List<List<Integer>> answer = Arrays.asList(new ArrayList<>(distinctIntegersInNums1), new ArrayList<>(distinctIntegersInNums2));
-        // answer.add(new ArrayList<>(x));
-        // answer.add(new ArrayList<>(distinctIntegersInNums2.removeAll(distinctIntegersInNums1)));
         return answer;
     }
 }
