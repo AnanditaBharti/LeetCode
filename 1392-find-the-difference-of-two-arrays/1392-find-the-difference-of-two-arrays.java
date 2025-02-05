e17ce2b9f7ecc540d87faa4a96e15a767de15f63
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2025-02-05 17:28:50 +0530

    Time: 19 ms (14.83%), Space: 46.1 MB (5.49%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.java
@@ -1,23 +1,29 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         // List<Integer> distinctIntegersInNums1 = Arrays.asList(nums1);
-        Set<Integer> distinctIntegersInNums1 = Arrays.stream(nums1) 
+        Set<Integer> integersInNums1 = Arrays.stream(nums1) 
                                                        .boxed()
                                                        .collect(Collectors.toSet()); 
-        Set<Integer> distinctIntegersInNums2 = Arrays.stream(nums2) 
+        Set<Integer> integersInNums2 = Arrays.stream(nums2) 
                                                        .boxed()
                                                        .collect(Collectors.toSet());
-        Iterator<Integer> iterate = distinctIntegersInNums1.iterator() ;
-        while(iterate.hasNext()){
-            Integer value = iterate.next() ;
-            if(distinctIntegersInNums2.contains(value)){
-                iterate.remove();
-                distinctIntegersInNums2.remove(value);
-            }
-        }
-        List<List<Integer>> answer = new ArrayList<>() ;
-        answer.add(new ArrayList<>(distinctIntegersInNums1));
-        answer.add(new ArrayList<>(distinctIntegersInNums2));
+        // Iterator<Integer> iterate = distinctIntegersInNums1.iterator() ;
+        // while(iterate.hasNext()){
+        //     Integer value = iterate.next() ;
+        //     if(distinctIntegersInNums2.contains(value)){
+        //         iterate.remove();
+        //         distinctIntegersInNums2.remove(value);
+        //     }
+        // }
+        Set<Integer> distinctIntegersInNums1 = new HashSet<>(integersInNums1);
+        Set<Integer> distinctIntegersInNums2 = new HashSet<>(integersInNums2);
+        distinctIntegersInNums1.removeAll(integersInNums2) ;
+        distinctIntegersInNums2.removeAll(integersInNums1) ;
+        // List<Integer> x = new ArrayList<>(distinctIntegersInNums1) ;
+        // List<Integer> y = new ArrayList<>(distinctIntegersInNums2) ;
+        List<List<Integer>> answer = Arrays.asList(new ArrayList<>(distinctIntegersInNums1), new ArrayList<>(distinctIntegersInNums2));
+        // answer.add(new ArrayList<>(x));
+        // answer.add(new ArrayList<>(distinctIntegersInNums2.removeAll(distinctIntegersInNums1)));
         return answer;
     }
 }
