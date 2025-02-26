b719ac35a384321f4ddfe5ef884238cd354831d4
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2025-02-26 11:56:29 +0530

    Time: 2 ms (62.32%), Space: 44.3 MB (75.45%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,33 +14,31 @@
  * }
  */
 class Solution {
-    public TreeNode helpBuildTree(int[] inorder,int startIn, int endIn, int[] postorder, int indexPost){
+    public TreeNode helpBuildTree(int[] inorder,int startIn, int endIn, int[] postorder, int indexPost, Map<Integer,Integer> mapValueToIndex){
         if (startIn > endIn || indexPost < 0) {
             return null;
         }
         int val = postorder[indexPost] ;
         TreeNode root = new TreeNode (val) ;
-        int splitIndex = -1 ;
-        for (int i = startIn; i <= endIn; i++) {
-            if (inorder[i] == val){
-                splitIndex = i ;
-                break;
-            }
-        }
-        // System.out.println(val+","+splitIndex);
-        // indexPost--;
-        if ( (indexPost - 1 - (endIn - splitIndex )) >= 0)
-            System.out.println(val+" - "+ postorder[ indexPost - 1 - (endIn - splitIndex )]);
-        root.right = helpBuildTree(inorder, splitIndex + 1, endIn, postorder, indexPost - 1) ;
-        // if (root.right != null)
-        //     indexPost--;
-        root.left = helpBuildTree(inorder, startIn, splitIndex - 1, postorder, indexPost - 1 - (endIn - splitIndex )) ;
+        int splitIndex = mapValueToIndex.get(val) ;
+        // for (int i = startIn; i <= endIn; i++) {
+        //     if (inorder[i] == val){
+        //         splitIndex = i ;
+        //         break;
+        //     }
+        // }
+        root.right = helpBuildTree(inorder, splitIndex + 1, endIn, postorder, indexPost - 1, mapValueToIndex) ;
+        root.left = helpBuildTree(inorder, startIn, splitIndex - 1, postorder, indexPost - 1 - (endIn - splitIndex ), mapValueToIndex) ;
         return root;
 
     }
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         if (inorder == null || postorder == null)
             return null;
-        return helpBuildTree(inorder, 0, inorder.length - 1, postorder, postorder.length - 1) ;
+        Map<Integer,Integer> mapValueToIndex = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            mapValueToIndex.put(inorder[i],i);
+        }
+        return helpBuildTree(inorder, 0, inorder.length - 1, postorder, postorder.length - 1, mapValueToIndex) ;
     }
 }
