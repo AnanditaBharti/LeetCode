7b2052008a63cae83c09ba0dcf81f4a8353e586f
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2025-03-04 12:24:42 +0530

    Time: 139 ms (5.25%), Space: 43.3 MB (60.25%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -4,20 +4,38 @@ class Solution {
             res.add(currString);
             return;
         }
-        // StringBuilder temp = currString;
         if (open < n) {
             helper (new StringBuilder(currString + "("), open + 1, close, n, res) ;
-            // temp = currString;
         }
         if (close < open) {
             helper (new StringBuilder(currString + ")"), open, close + 1, n, res) ;
         }
         
     }
+
+
     public List<String> generateParenthesis(int n) {
-        List<StringBuilder> res = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        helper(sb, 0, 0, n, res);
-        return res.stream().map(StringBuilder::toString).toList();
+        // List<StringBuilder> res = new ArrayList<>();
+        // StringBuilder sb = new StringBuilder();
+        // helper(sb, 0, 0, n, res);
+        // return res.stream().map(StringBuilder::toString).toList();
+
+        List<List<String>> validParenthesisAtEachIndex = new ArrayList<>();
+        validParenthesisAtEachIndex.add(new ArrayList<>());
+        validParenthesisAtEachIndex.get(0).add("");
+
+        for (int i = 1; i <= n; i++) {
+            List<String> temp = new ArrayList<>();
+            for (int j = 0; j < validParenthesisAtEachIndex.size(); j++) {
+                for (String k : validParenthesisAtEachIndex.get(j)) {
+                    for (String l : validParenthesisAtEachIndex.get(i - j - 1)) {
+                        temp.add( "(" + k + ")" + l );
+                    }
+                    
+                }
+            }
+            validParenthesisAtEachIndex.add(temp);
+        }
+        return validParenthesisAtEachIndex.get(n);
     }
 }
