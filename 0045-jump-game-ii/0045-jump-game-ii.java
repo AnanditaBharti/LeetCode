2deb455018b2f4d369b5c2c1a4feec5d63db6f4a
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2025-03-05 14:21:44 +0530

    Time: 1 ms (99.46%), Space: 44.8 MB (93.23%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,17 +1,34 @@
 class Solution {
     public int jump(int[] nums) {
+        
         if (nums == null)
             return 0;
         int n = nums.length;
-        int[] minSteps = new int[n];
-        for (int i = 1; i < n; i++) {
-            minSteps[i] = Integer.MAX_VALUE;
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j <= (i + nums[i]) && j < n; j++) {
-                minSteps[j] = Math.min(minSteps[j], minSteps[i] + 1);
+
+        // dynamic programming approach
+
+        // int[] minSteps = new int[n];
+        // for (int i = 1; i < n; i++) {
+        //     minSteps[i] = Integer.MAX_VALUE;
+        // }
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i+1; j <= (i + nums[i]) && j < n; j++) {
+        //         minSteps[j] = Math.min(minSteps[j], minSteps[i] + 1);
+        //     }
+        // }
+        // return minSteps[n - 1];
+
+        int farthest = 0;
+        int currJump = 0;
+        int jumps = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i >= currJump) {
+                jumps ++;
+                currJump = farthest;
             }
         }
-        return minSteps[n - 1];
+        return jumps;
     }
 }
