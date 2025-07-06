492325563ea8f74336e0aed2df15749b20579565
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2025-07-06 19:27:41 +0530

    Time: 159 ms (29.82%), Space: 75.5 MB (44.21%) - LeetHub

---
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
@@ -0,0 +1,60 @@
+class FindSumPairs {
+    int[] nums1;
+    int[] nums2;
+    HashMap<Integer, Integer> ele;
+    public FindSumPairs(int[] nums1, int[] nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        ele = new HashMap<>();
+        for(int i = 0; i < nums2.length; i++){
+            ele.put(nums2[i], ele.getOrDefault(nums2[i], 0) + 1);
+        }
+    }
+    
+    public void add(int index, int val) {
+        int oldVal = nums2[index];
+        nums2[index] += val;
+        ele.put(nums2[index], ele.getOrDefault(nums2[index], 0) + 1);
+        if(ele.get(oldVal) > 1){
+            ele.put(oldVal, ele.get(oldVal) - 1);
+        }
+        else{
+            ele.remove(oldVal);
+        }
+    }
+    
+    public int count(int tot) {
+        int c = 0;
+        for(int i = 0; i < nums1.length; i++){
+            int temp = tot - nums1[i];
+            if(ele.containsKey(temp)){
+                c += ele.get(temp);
+            }
+        }
+        return c;
+
+        // HashMap<Integer, Integer> ele = new HashMap<>();
+        // for(int i = 0; i < nums1.length; i++){
+        //     int temp = tot - nums1[i];
+        //     if(ele.containsKey(temp)){
+        //         ele.put(temp, ele.get(temp) + 1);
+        //     }
+        //     else{
+        //         ele.put(temp, 1);
+        //     }
+        // }
+        // for(int j = 0; j < nums2.length; j++){
+        //     if(ele.containsKey(nums2[j])){
+        //         c += ele.get(nums2[j]);
+        //     }
+        // }
+        // return c;
+    }
+}
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs obj = new FindSumPairs(nums1, nums2);
+ * obj.add(index,val);
+ * int param_2 = obj.count(tot);
+ */
