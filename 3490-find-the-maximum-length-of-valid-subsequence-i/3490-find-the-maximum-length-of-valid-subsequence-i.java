dd83fab6dc2d05aafa23a2a034312f0401705073
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2025-07-17 21:46:01 +0530

    Time: 12 ms (21.77%), Space: 62 MB (89.52%) - LeetHub

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int oddSubsequenceLength(int[] nums){
+        int n = nums.length;
+        int len = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] % 2 == 1){
+                len ++;
+            }
+        }
+        return len;
+    }
+    public int evenSubsequenceLength(int[] nums){
+        int n = nums.length;
+        int len = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] % 2 == 0){
+                len ++;
+            }
+        }
+        return len;
+    }
+    public int mixSubsequenceLength(int[] nums){
+        int n = nums.length;
+        int len = 1;
+        int turn = (nums[0] % 2 == 0) ? 1 : 0;
+        for(int i = 1; i < n; i++){
+            if(nums[i] % 2 == turn){
+                len ++;
+                turn = (turn == 0) ? 1 : 0;
+            }
+        }
+        return len;
+    }
+    public int maximumLength(int[] nums) {
+        int n = nums.length;
+        // int dp[][] = new int[n][2];
+        // int res = 0;
+        // for(int i = 0; i < n; i++){
+// 
+        // }
+        return Math.max(oddSubsequenceLength(nums),
+        Math.max(evenSubsequenceLength(nums),mixSubsequenceLength(nums)));
+    }
+}
