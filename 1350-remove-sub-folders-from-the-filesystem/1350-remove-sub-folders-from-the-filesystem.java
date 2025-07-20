ab6a81a5e045f63ae7abf9967320f00fead7c0d4
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2025-07-20 11:53:00 +0530

    Time: 39 ms (87.45%), Space: 55.1 MB (91.83%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.java
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.java
@@ -1,17 +1,23 @@
 class Solution {
     public List<String> removeSubfolders(String[] folder) {
         Arrays.sort(folder);
-        HashSet<String> folders = new HashSet<>();
+        // HashSet<String> folders = new HashSet<>();
+        ArrayList<String> folders = new ArrayList<>();
+        String prev = "";
         for(int i = 0; i < folder.length; i++){
-            if(folder[i] != null){
+            if(prev.isEmpty() || !folder[i].startsWith(prev + "/")){
                 folders.add(folder[i]);
-                for(int j = i + 1; j < folder.length; j++){
-                    if(folder[j] != null && folder[j].startsWith(folder[i] + "/")){
-                        folder[j] = null;
-                    }
-                }
+                prev = folder[i];
             }
+            // if(folder[i] != null){
+            //     folders.add(folder[i]);
+            //     for(int j = i + 1; j < folder.length; j++){
+            //         if(folder[j] != null && folder[j].startsWith(folder[i] + "/")){
+            //             folder[j] = null;
+            //         }
+            //     }
+            // }
         }
-        return folders.stream().toList();
+        return folders;
     }
 }
