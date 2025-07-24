20a37c0c9fe6e03a7229e0490fe3347299b05023
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2025-07-24 20:22:37 +0530

    Time: 48 ms (68.85%), Space: 57.2 MB (89.12%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.java
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.java
@@ -1,22 +1,27 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
-        int left = 0, right = 0;
-        int maxSum = 0, currentSum = 0;
-        HashSet<Integer> set = new HashSet<>();
+        HashMap<Integer,Integer> eleIndex = new HashMap<>();
+        int maxSum = 0;
+        int tempSum = 0;
+        int startPtr = 0;
+        int n = nums.length;
+        int cummulativeSum[] = new int[n];
+        cummulativeSum[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            cummulativeSum[i] = cummulativeSum[i - 1] + nums[i];
+        }
+        for(int i = 0; i < n; i++){
+            if(eleIndex.containsKey(nums[i]) && eleIndex.get(nums[i]) >= startPtr){
+                startPtr = eleIndex.get(nums[i]) + 1;
+                eleIndex.put(nums[i], i);
 
-        while (right < nums.length) {
-            if (!set.contains(nums[right])) {
-                set.add(nums[right]);
-                currentSum += nums[right];
-                maxSum = Math.max(maxSum, currentSum);
-                right++;
-            } else {
-                set.remove(nums[left]);
-                currentSum -= nums[left];
-                left++;
+            }
+            else{
+                eleIndex.put(nums[i], i);
+                tempSum = (startPtr == 0) ? cummulativeSum[i] : cummulativeSum[i] - cummulativeSum[startPtr - 1];
+                maxSum = (tempSum > maxSum) ? tempSum : maxSum;
             }
         }
-
         return maxSum;
     }
 }
