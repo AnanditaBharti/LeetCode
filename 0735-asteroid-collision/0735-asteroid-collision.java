d39216d0bdfaf972c509943441ae8850f3f1653b
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2025-08-24 16:25:29 +0530

    Time: 5 ms (84.11%), Space: 45.6 MB (34.51%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -36,46 +36,5 @@ class Solution {
             j--;
         }
         return res;
-
-        // Deque<Integer> stack = new ArrayDeque<>();
-        // for(int i = 0; i < asteroids.length; i++){
-        //     System.out.print(i + "[");
-        //     int currEle = asteroids[i]; //-2
-        //     if(stack.isEmpty()){
-        //         stack.push(currEle);
-        //         System.out.println("]");
-        //         continue;
-        //     }
-        //     if(currEle < 0){
-        //         int preEle = stack.peek(); //1
-
-        //         // System.out.println(preEle);
-
-        //         while(preEle > 0){
-        //             while(Math.abs(currEle) >= preEle){
-        //                 stack.pop();
-        //                 if(stack.isEmpty()){
-        //                     break;
-        //                 }
-        //                 preEle = stack.peek();
-        //             }
-        //         }
-        //         else{
-        //             stack.push(currEle);
-        //         }
-        //     }
-        //     else {
-        //         stack.push(currEle);
-        //     }
-        //     stack.stream().forEach(ele -> System.out.print(ele+ ","));
-        //     System.out.println("]");
-        // }
-        // int[] res = new int[stack.size()];
-        // int j = stack.size() - 1;
-        // for(Integer i: stack){
-        //     res[j] = i;
-        //     j--;
-        // }
-        // return res;
     }
 }
