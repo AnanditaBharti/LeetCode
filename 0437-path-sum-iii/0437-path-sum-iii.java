6f8e797b52761b553581cc1b5a7b0fa729bae585
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2025-10-11 22:32:52 +0530

    Time: 3 ms (96.43%), Space: 44.4 MB (37.96%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -14,26 +14,47 @@
  * }
  */
 class Solution {
-    long dfs(TreeNode root, int targetSum, long currSum){
+    // long dfs(TreeNode root, int targetSum, long currSum){
+        // if(root == null){
+        //     return 0;
+        // }
+    //     int count = 0;
+    //     currSum += root.val;
+    //     if(currSum == targetSum)
+    //         count = 1;
+    //     count += dfs(root.left, targetSum, currSum);
+    //     count += dfs(root.right, targetSum, currSum);
+    //     return count;
+    // }
+    int dfs(TreeNode root, int targetSum, long currSum, HashMap<Long, Integer> freq){
         if(root == null){
             return 0;
         }
-        int count = 0;
         currSum += root.val;
-        if(currSum == targetSum)
-            count = 1;
-        count += dfs(root.left, targetSum, currSum);
-        count += dfs(root.right, targetSum, currSum);
+        int count = 0;
+        count += freq.getOrDefault(currSum - targetSum, 0);
+        freq.put(currSum, freq.getOrDefault(currSum, 0) + 1);
+        count += dfs(root.left, targetSum, currSum, freq);
+        count += dfs(root.right, targetSum, currSum, freq);
+
+        freq.put(currSum, freq.get(currSum) - 1);
         return count;
+
+        // if(freq.containsKey(currSum - targetSum)){
+        //     count += freq.getOrDEf
+        // }
     }
     public int pathSum(TreeNode root, int targetSum) {
-        if(root == null){
-            return 0;
-        }
-        int count = 0;
-        count += dfs(root, targetSum, 0);
-        count += pathSum(root.left, targetSum);
-        count += pathSum(root.right, targetSum);
-        return count;
+        HashMap<Long, Integer> freq = new HashMap<>();
+        freq.put(0l, 1);
+        return dfs(root, targetSum, 0, freq);
+    //     if(root == null){
+    //         return 0;
+    //     }
+    //     int count = 0;
+    //     count += dfs(root, targetSum, 0);
+    //     count += pathSum(root.left, targetSum);
+    //     count += pathSum(root.right, targetSum);
+    //     return count;
     }
 }
