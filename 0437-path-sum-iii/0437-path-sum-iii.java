fd966e72fb95e314d6eb6d425fb29cd73c4abc2e
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2025-10-11 22:33:30 +0530

    Time: 3 ms (96.43%), Space: 44.8 MB (9.48%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -27,34 +27,21 @@ class Solution {
     //     return count;
     // }
     int dfs(TreeNode root, int targetSum, long currSum, HashMap<Long, Integer> freq){
-        if(root == null){
+        if(root == null)
             return 0;
-        }
         currSum += root.val;
         int count = 0;
         count += freq.getOrDefault(currSum - targetSum, 0);
         freq.put(currSum, freq.getOrDefault(currSum, 0) + 1);
         count += dfs(root.left, targetSum, currSum, freq);
         count += dfs(root.right, targetSum, currSum, freq);
-
         freq.put(currSum, freq.get(currSum) - 1);
         return count;
 
-        // if(freq.containsKey(currSum - targetSum)){
-        //     count += freq.getOrDEf
-        // }
     }
     public int pathSum(TreeNode root, int targetSum) {
         HashMap<Long, Integer> freq = new HashMap<>();
         freq.put(0l, 1);
         return dfs(root, targetSum, 0, freq);
-    //     if(root == null){
-    //         return 0;
-    //     }
-    //     int count = 0;
-    //     count += dfs(root, targetSum, 0);
-    //     count += pathSum(root.left, targetSum);
-    //     count += pathSum(root.right, targetSum);
-    //     return count;
     }
 }
