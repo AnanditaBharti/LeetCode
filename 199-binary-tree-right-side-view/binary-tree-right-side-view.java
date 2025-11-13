e9065aa6a73886e52f68578c3d0d3f78202f9056
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2025-11-13 15:47:09 +0530

    Time: 1 ms (71.21%) | Memory: 43.7 MB (7.02%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // LinkedHashMap<Integer, Integer> bfsTree = new LinkedHashMap<>();
+    List<Integer> ans = new ArrayList<>();
+    public void bfs(List<TreeNode> currLevelNodes){
+        if(currLevelNodes.isEmpty())
+            return;
+        List<TreeNode> nextLevelNodes = new ArrayList<>();
+        TreeNode rightMost = null;
+        for(TreeNode t: currLevelNodes){
+            if(t == null)
+                continue;
+            rightMost = t;
+            if(t.left != null){
+                nextLevelNodes.add(t.left);
+            }
+            if(t.right != null){
+                nextLevelNodes.add(t.right);
+            }
+        }
+        if(rightMost != null)
+            ans.add(rightMost.val);
+        bfs(nextLevelNodes);
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        List<TreeNode> currLevelNodes = new ArrayList<>();
+        currLevelNodes.add(root);
+        bfs(currLevelNodes);
+        return ans;
+    }
+}
