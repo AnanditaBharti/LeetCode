ef060f3505724f879ae0907dc72fb5ceb6e4f387
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2026-02-24 12:15:32 +0530

    Time: 43 ms (97.79%) | Memory: 72.2 MB (97.34%) - LeetSync

--- 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.java
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.java
@@ -1,37 +1,77 @@
 class Solution {
     public long totalCost(int[] costs, int k, int candidates) {
+
         int n = costs.length;
-        PriorityQueue<Integer> leftMinHeap = new PriorityQueue<>(
-            (a,b) -> Integer.compare(costs[a], costs[b])
-        );
-        PriorityQueue<Integer> rightMinHeap = new PriorityQueue<>(
-            (a,b) -> Integer.compare(costs[a], costs[b])
-        );
+
+        if(2 * candidates >= n){
+            Arrays.sort(costs);
+            return Arrays.stream(costs).limit(k).sum();
+        }
+
+        PriorityQueue<Integer> leftMinHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> rightMinHeap = new PriorityQueue<>();
+        int left = 0;
+        int right = n - 1;
+
+        for(int i = 0; i < candidates; i++){
+            leftMinHeap.offer(costs[left++]);
+        }
+        for(int i = 0; i < candidates; i++){
+            rightMinHeap.offer(costs[right--]);
+        }
+
         long totalCost = 0;
-        int l = 0;
-        int r = n - 1;
+
         for(int i = 0; i < k; i++){
-            while(l <= r && leftMinHeap.size() < candidates){
-                leftMinHeap.offer(l);
-                l ++;
-            }
-            while(l <= r && rightMinHeap.size() < candidates){
-                rightMinHeap.offer(r);
-                r --;
-            }
+            if(rightMinHeap.isEmpty() || (!leftMinHeap.isEmpty() && (leftMinHeap.peek() <= rightMinHeap.peek()))){
 
-            int lowestCost = 0;
-            int first = (leftMinHeap.peek() == null) ? Integer.MAX_VALUE : costs[leftMinHeap.peek()];
-            int last = (rightMinHeap.peek() == null) ? Integer.MAX_VALUE : costs[rightMinHeap.peek()];
+                totalCost += leftMinHeap.poll();
 
-            if(first <= last){
-                lowestCost = leftMinHeap.poll();
+                if(left <= right)
+                    leftMinHeap.offer(costs[left++]);
             }
             else{
-                lowestCost = rightMinHeap.poll();
+                totalCost += rightMinHeap.poll();
+
+                if(left <= right)
+                    rightMinHeap.offer(costs[right--]);
             }
-            totalCost += costs[lowestCost];
         }
         return totalCost;
+
+
+        // int n = costs.length;
+        // PriorityQueue<Integer> leftMinHeap = new PriorityQueue<>(
+        //     (a,b) -> Integer.compare(costs[a], costs[b])
+        // );
+        // PriorityQueue<Integer> rightMinHeap = new PriorityQueue<>(
+        //     (a,b) -> Integer.compare(costs[a], costs[b])
+        // );
+        // long totalCost = 0;
+        // int l = 0;
+        // int r = n - 1;
+        // for(int i = 0; i < k; i++){
+        //     while(l <= r && leftMinHeap.size() < candidates){
+        //         leftMinHeap.offer(l);
+        //         l ++;
+        //     }
+        //     while(l <= r && rightMinHeap.size() < candidates){
+        //         rightMinHeap.offer(r);
+        //         r --;
+        //     }
+
+        //     int lowestCost = 0;
+        //     int first = (leftMinHeap.peek() == null) ? Integer.MAX_VALUE : costs[leftMinHeap.peek()];
+        //     int last = (rightMinHeap.peek() == null) ? Integer.MAX_VALUE : costs[rightMinHeap.peek()];
+
+        //     if(first <= last){
+        //         lowestCost = leftMinHeap.poll();
+        //     }
+        //     else{
+        //         lowestCost = rightMinHeap.poll();
+        //     }
+        //     totalCost += costs[lowestCost];
+        // }
+        // return totalCost;
     }
 }
