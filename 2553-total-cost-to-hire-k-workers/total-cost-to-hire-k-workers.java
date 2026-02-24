0054593dc154ae44c11c1dad98f4875515066e7e
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2026-02-24 11:33:51 +0530

    Time: 68 ms (26.99%) | Memory: 72.1 MB (98.85%) - LeetSync

---
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.java
@@ -0,0 +1,57 @@
+class Solution {
+    public long totalCost(int[] costs, int k, int candidates) {
+        int n = costs.length;
+        PriorityQueue<Integer> leftMinHeap = new PriorityQueue<>(
+            (a,b) -> Integer.compare(costs[a], costs[b])
+        );
+        PriorityQueue<Integer> rightMinHeap = new PriorityQueue<>(
+            (a,b) -> Integer.compare(costs[a], costs[b])
+        );
+        long totalCost = 0;
+        int l = 0;
+        int r = n - 1;
+        for(int i = 0; i < k; i++){
+            while(l <= r && leftMinHeap.size() < candidates){
+                leftMinHeap.offer(l);
+                l ++;
+            }
+            while(l <= r && rightMinHeap.size() < candidates){
+                rightMinHeap.offer(r);
+                r --;
+            }
+
+            int lowestCost = 0;
+            int first = (leftMinHeap.peek() == null) ? Integer.MAX_VALUE : costs[leftMinHeap.peek()];
+            int last = (rightMinHeap.peek() == null) ? Integer.MAX_VALUE : costs[rightMinHeap.peek()];
+
+            if(first <= last){
+                lowestCost = leftMinHeap.poll();
+                // l ++;
+            }
+            // else if(first == last){
+            //     if(leftMinHeap.peek() == rightMinHeap.peek()){
+            //         lowestCost = leftMinHeap.poll();
+            //         rightMinHeap.poll();
+            //         // l ++;
+            //         // r --;
+            //     }
+            //     else if(first < last){
+            //         lowestCost = leftMinHeap.poll();
+            //         // l ++;
+            //     }
+            //     else{
+            //         lowestCost = rightMinHeap.poll();
+            //         // r --;
+            //     }
+            // }
+            else{
+                lowestCost = rightMinHeap.poll();
+                // r --;
+            }
+            // if(l >= )
+            totalCost += costs[lowestCost];
+            // System.out.println("i = " + i + " -> " + costs[lowestCost]);
+        }
+        return totalCost;
+    }
+}
