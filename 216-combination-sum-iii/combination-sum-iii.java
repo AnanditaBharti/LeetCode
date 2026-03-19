c1fd296b6cb89422bb5fd4b34b803478019deb40
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2026-03-19 15:04:16 +0530

    Time: 1 ms (24.61%) | Memory: 42.5 MB (61.19%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -0,0 +1,21 @@
+class Solution {
+    void backTrack(int k, int n, List<Integer> curr, List<List<Integer>> list){
+        if(k == 0 && n == 0){
+            list.add(new LinkedList<>(curr));
+            return;
+        }
+        else if (k != 0 && n != 0){
+            // int last = (curr.isEmpty()) ? 1 : curr.get(curr.size() - 1) + 1 ;
+            for(int i = (curr.isEmpty()) ? 1 : curr.get(curr.size() - 1) + 1; i < 10; i ++){
+                curr.add(i);
+                backTrack(k - 1, n - i, curr, list);
+                curr.remove(curr.size() - 1);
+            }
+        }
+    }
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new LinkedList<>();
+        backTrack(k, n, new LinkedList<>(), result);
+        return result;
+    }
+}
