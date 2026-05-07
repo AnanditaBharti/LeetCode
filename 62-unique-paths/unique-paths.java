091cd456a35df23290fdbaa3a1592022fd1d0595
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2026-05-07 15:50:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (28.37%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,21 +1,5 @@
 class Solution {
-    // void helper(int m, int n, int[][] dp, int r, int c){
-    //     if(r < m - 1){
-    //         if(dp[r + 1][c] == 0)
-    //             helper(m, n, dp, r + 1, c);
-    //         dp[r][c] += dp[r + 1][c];
-    //     }
-    //     if(c < n - 1){
-    //         if(dp[r][c + 1] == 0)
-    //             helper(m, n, dp, r, c + 1);
-    //         dp[r][c] += dp[r][c + 1];
-    //     }
-    // }
     public int uniquePaths(int m, int n) {
-        // int[][] dp = new int[m][n];
-        // dp[m - 1][n - 1] = 1;
-        // helper(m, n, dp, 0, 0);
-        // return dp[0][0];
         int a = m + n - 2;
         int b = Math.min(m - 1, n - 1);
         long res = 1;
