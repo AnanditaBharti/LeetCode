311f4625679869e5386237962fe7dd1c4794d69e
Anandita Bharti <141313454+AnanditaBharti@users.noreply.github.com>
2026-09-15 15:00:47 +0530

    Time: 13 ms (82.74%) | Memory: 48.9 MB (75.21%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.java
+++ 1397-search-suggestions-system/search-suggestions-system.java
@@ -1,20 +1,5 @@
 class Solution {
-    // public void dfs(ArrayDeque<TrieNode> stack, StringBuilder prefix, String searchWord, int index, List<String> products){
-    //     if(stack.isEmpty())
-    //         return;
-    //     TrieNode node = stack.removeLast();
-    //     if(node.isEndOfWord){
-    //         products.add(prefix.toString());
-    //         if(products.size() >= 3)
-    //             return;
-    //     }
-    //     dfs(stack, )
-    //     // for(int i = 0; i < 26; i++){
-    //     //     if(node.children[i] != null){
-    //     //         stack.add(node.children[i]);
-    //     //     }
-    //     // }
-    // }
+
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
 
         Arrays.sort(products);
@@ -38,73 +23,16 @@ class Solution {
             }
             finalSuggestions.add(temp.suggestions);
         }
-
         return finalSuggestions;
-        // List<TrieNode> list = new ArrayList<>();
-        // list.add(trie.root);
-        // int level = 1;
-        // int count = 0;
-        // while(!list.isEmpty()){
-        //     TrieNode t = list.remove(0);
-        //     for(int i = 0; i < 26; i++){
-        //         if(t.children[i] != null){
-        //             System.out.print((char)('a' + i) + ", ");
-        //             list.add(t.children[i]);
-        //             count ++;
-        //         }
-        //     }
-        //     level --;
-        //     if(level == 0){
-        //         level = count;
-        //         count = 0;
-        //         System.out.println();
-        //     }
-
-        // }
-
-        // List<List<String>> output = new ArrayList<>();
-        // TrieNode node = trie.root;
-        // for(char c: searchWord.toCharArray()){
-        //     List<String> curr = new ArrayList<>();
-        // }
-
-        // List<List<String>> res = new ArrayList<>();
-        // StringBuilder prefix = new StringBuilder();
-
-        // for(int i = 0; i < searchWord.length(); i++){
-        //     prefix.append(searchWord.charAt(i));
-        //     count = 0;
-        //     TrieNode t = root.root;
-        //     int j = i;
-        //     List<String> prod = new ArrayList<>();
-        //     StringBuilder prefix2 = new StringBuilder();
-        //     prefix2.append(prefix);
-        //     while(count < 3){
-        //         if (t.isEndOfWord){
-        //             prod.add(prefix2.toString());
-        //             count ++;
-        //         }
-        //         int index = searchWord.charAt(j) - 'a';
-        //         prefix2.append(prefix2);
-        //         t = t.children[index];
-        //         if(t == null)
-        //             break;
-                
-        //     }
-        //     res.add(prod);
-            
-        // }
-        // return res;
     }
 }
 class TrieNode{
     TrieNode[] children;
-    boolean isEndOfWord;
     List<String> suggestions;
 
     public TrieNode(){
         children = new TrieNode[26];
-        suggestions = new ArrayList();
+        suggestions = new ArrayList<>(3);
     }
 }
 class Trie{
@@ -114,8 +42,6 @@ class Trie{
     }
     void insert(String word){
         TrieNode temp = root;
-        // if(temp.suggestions.length() < 3)
-        //     temp.suggestions.add(word);
         for(char c: word.toCharArray()){
             int index = c - 'a';
             if(temp.children[index] == null){
@@ -125,7 +51,5 @@ class Trie{
                 temp.children[index].suggestions.add(word);
             temp = temp.children[index];
         }
-        temp.isEndOfWord = true;
     }
-
 }
